9defaa95077f54477426aa494f5d3b13aaf269f1
joelson91 <joelsonandradep@gmail.com>
2024-05-19 17:10:50 -0300

    feat: Add cryptocurrency price viewer app with selection options and chart
    This commit adds a new feature to the application, allowing users to view cryptocurrency prices. It includes the following changes:
    - Added a title to the application
    - Added selection options for choosing a cryptocurrency and a currency
    - Added a slider for selecting the number of days
    - Created a table based on the selected options
    - Added a price chart for the selected cryptocurrency
    
    These changes enhance the functionality of the application and provide users with a better experience in visualizing cryptocurrency prices.

--- model.py
+++ model.py
@@ -52,5 +52,23 @@ class Tabela:
         return df
 
 if __name__ == "__main__":
-    tabela = Tabela("bitcoin", "usd", "7")
-    st.dataframe(tabela.dataframe)
+
+    # Título do aplicativo
+    st.title("Visualizador de Preços de Criptomoedas")
+
+    # Opções de seleção
+    moedas = ["usd", "eur", "brl"]
+    criptomoedas = ["bitcoin", "ethereum", "cardano", "solana", "polkadot"]
+
+    # Widgets de seleção
+    criptomoeda = st.selectbox("Selecione uma criptomoeda", criptomoedas, index=0, key="criptomoeda", help="Selecione a criptomoeda que deseja visualizar")
+    moeda_selecionada = st.selectbox("Selecione uma moeda", moedas, index=2, key="moeda", help="Selecione a moeda que deseja visualizar")
+    dias_selection = st.slider("Selecione o número de dias", 1, 365, 7)
+
+    # Criação da tabela
+    tabela = Tabela(criptomoeda , moeda_selecionada, dias_selection)
+
+    # Gráfico de preços
+    if criptomoeda is not None:
+        st.title(f"Gráfico de Preços do {criptomoeda.capitalize()}")
+        st.line_chart(tabela.dataframe.set_index('date'))
