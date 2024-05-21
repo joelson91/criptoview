830fe1fbb8a4bc5df44ebfaf65d855710d8036fd
joelson91 <joelsonandradep@gmail.com>
2024-05-21 16:35:14 -0300

    chore: Refactor wallet page to include transaction form and transaction list
    This commit refactors the wallet page to include a transaction form and a transaction list. It adds the following changes:
    - Renamed the page title to "Formulário de Transações"
    - Created a form for adding transactions with fields for cryptocurrency, date, value, and type
    - Added a submit button to the form
    - Implemented the logic to add transactions to the wallet
    - Displayed the list of transactions in the "Lista de Transações" section
    - Calculated and displayed the wallet's earnings in the "Rendimento" section
    
    These changes improve the functionality of the wallet page by allowing users to add and view their transactions easily.

--- pages/wallet.py
+++ pages/wallet.py
@@ -1,5 +1,25 @@
 import streamlit as st
-from model import Tabela
+from model import Carteira
 
-# Título do aplicativo
-st.title("Minha carteira")
+# Formulário de transações
+st.title("Formulário de Transações")
+
+# Janela de popup para adicionar transação
+carteira = Carteira()
+with st.form("Adicionar Transação"):
+    criptomoeda = st.text_input('Criptomoeda')
+    data = st.date_input('Data')
+    valor = st.number_input('Valor')
+    tipo = st.selectbox('Tipo', ['compra', 'venda'])
+    submit_button = st.form_submit_button(label='Adicionar Transação')
+    if submit_button:
+        carteira.adicionar_transacao(criptomoeda, data, valor, tipo)
+
+# Listar transações
+st.title("Lista de Transações")
+for transacao in carteira.transacoes:
+    st.write(transacao)
+
+# Calcular e exibir rendimento
+rendimento = carteira.calcular_rendimento()
+st.write(f'Rendimento: {rendimento}')
